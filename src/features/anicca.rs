559be3dec6067b6622ad1b2b3671d91e753a081d
xaviduds <xaviduds@gmail.com>
2025-03-16 17:20:58 -0300

    tidying

--- src/features/anicca.rs
+++ src/features/anicca.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use dirs;
 use serde::Deserialize;
 use std::io::ErrorKind;
@@ -18,38 +19,50 @@ pub fn anicca() -> std::io::Result<()> {
     let config_path = String::from(config_dir.to_str().unwrap()) + "/lince/os.toml";
     let config_contents: Config = toml::from_str(&fs::read_to_string(config_path)?).unwrap();
 
-    let nicca = &config_contents.nicca.list;
+    let persist_list = &config_contents.nicca.list;
 
     let mut map: HashMap<String, Vec<String>> = HashMap::new();
 
-    for line in nicca {
-        let pre = line.split("/");
-        let last = pre.clone().last().unwrap();
+    for line in persist_list {
+        let split_line = line.split("/");
+        let last_part = split_line.clone().last().unwrap();
 
-        let mut joined_pre = String::new();
+        let mut joined_line = String::new();
 
-        for part in pre {
-            if part != last {
-                joined_pre.push_str(part);
-                joined_pre.push_str("/");
+        for part in split_line {
+            if part != last_part {
+                joined_line.push_str(part);
+                joined_line.push_str("/");
             }
         }
-        map.entry(joined_pre).or_default().push(last.to_string());
+        map.entry(joined_line)
+            .or_default()
+            .push(last_part.to_string());
     }
     for (key, _values) in &map {
         let dir = fs::read_dir(key)?;
+
         for entry in dir {
-            let path = entry?.path();
-            let path_str = path.to_string_lossy();
-            if !nicca.contains(&path_str.to_string()) {
-                let _ = match fs::remove_dir_all(&*path_str) {
+            if entry.is_err() {
+                panic!(
+                    "Entry: {entry:?} caused the program to panic due to an error: {}",
+                    entry.as_ref().unwrap_err()
+                )
+            }
+
+            let path = entry.unwrap().path();
+
+            let path = path.to_string_lossy();
+
+            if !persist_list.contains(&path.to_string()) {
+                let _ = match fs::remove_dir_all(&*path) {
                     Ok(file) => file,
                     Err(error) => match error.kind() {
-                        ErrorKind::NotADirectory => match fs::remove_file(path_str.to_string()) {
+                        ErrorKind::NotADirectory => match fs::remove_file(path.to_string()) {
                             Ok(fd) => fd,
                             Err(e) => panic!("Problem deleting file: {e:?}"),
                         },
-                        ErrorKind::NotFound => println!("Error not found hayaa: {path_str}"),
+                        ErrorKind::NotFound => println!("Error not found hayaa: {path}"),
                         other_error => {
                             panic!("Other problem: {other_error:?}");
                         }
